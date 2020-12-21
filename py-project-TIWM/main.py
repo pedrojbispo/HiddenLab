e8bd9314c0c48ecebfe5723c03b1fd842ce5c48d
Ghost <55553435+PsiGhostPT@users.noreply.github.com>
2020-12-21 16:59:51 +0000

    Todo List
    +Todo List

--- py-project-TIWM/main.py
+++ py-project-TIWM/main.py
@@ -1,2 +1,11 @@
 #Gestao de Turma
+#
+# Methods: Add, Show, Remove, Modify
+# Files <- Dict{lst:lst}, Turmas, Alunos, Notas da turma
+# Calculo das Notas, Media
+# Aprovação dos aulos, Media > 10
+# Alunos: ID, Nome, Idade, DataNasc, Altura, Peso, Nacionalidade, Turma, Displina, Nota_Displina
+#
+
+
 
