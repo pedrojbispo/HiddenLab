64965701d6c0d214d9231fadd8bd78aec8e19c7a
Ghost <55553435+PsiGhostPT@users.noreply.github.com>
2021-01-12 20:46:46 +0000

    Dev #1.1
    From Dev #1 is missing
    +Added wait timers (time.sleep() function)
    For Dev #1.1
    -Remove os.system() calls for clean the screen
    +Added subprocess clear screen call

--- TIWM/Year1/TIWM-IntroProg/#py-project-TIWM/bin.py
+++ TIWM/Year1/TIWM-IntroProg/#py-project-TIWM/bin.py
@@ -1,22 +1,25 @@
-#-----------------#
+# -----------------#
 import os
 import time
 import platform
 import sys
 import psutil
-#-----------------#
+import subprocess as sp
+
+
+# -----------------#
 
 
 def clear():
     if platform.system() == "Windows":
-        os.system('cls')
+        tmp = sp.call('cls', shell=True)  # Windows call
     else:
-        os.system('clear')
+        tmp = sp.call('clear', shell=True) # *nix call
 
 
 print("Welcome to Bin.py, please wait...")
 time.sleep(5)
-print("="*40, "System Information", "="*40)
+print("=" * 40, "System Information", "=" * 40)
 uname = platform.uname()
 print(f"System: {uname.system}")
 print(f"Node Name: {uname.node}")
@@ -24,6 +27,6 @@ print(f"Release: {uname.release}")
 print(f"Version: {uname.version}")
 print(f"Machine: {uname.machine}")
 print(f"Processor: {uname.processor}")
-print("="*100)
+print("=" * 100)
 time.sleep(5)
 clear()
